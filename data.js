6271591883fa0d35085f2eb4fae7404050c5cd9e
SiRecon <78666759+SiRecon@users.noreply.github.com>
2021-02-07 09:43:36 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,267 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ekstra-kamar",
+      "name": "Ekstra Kamar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1749,
+      "initialViewParameters": {
+        "yaw": 3.104155600701132,
+        "pitch": 0.1807461011856457,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9466367700135052,
+          "pitch": 0.2862964986269425,
+          "rotation": 0,
+          "target": "2-halaman-luar"
+        },
+        {
+          "yaw": -0.30507672377181194,
+          "pitch": 0.30434898787756737,
+          "rotation": 0,
+          "target": "3-ruang-utama"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1310583151586266,
+          "pitch": 0.12237289130170836,
+          "title": "Ekstra Room",
+          "text": "Dapat digunakan tamu maupun asisten rumah anda.<div><br></div><div>Dekat dengan luar dan masih berjarak dengan ruangan inti rumah.</div>"
+        },
+        {
+          "yaw": -1.3075283780928508,
+          "pitch": -0.08145554817263445,
+          "title": "Akses Langsung",
+          "text": "Selain dapat dengan mudah mengawasi kondisi rumah.<div><br></div><div>Jika tamu merasa sungkan dapat langsung kabur dan dapat juga memudahkan untuk mengusir tamu dengan mudah ^_^</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-ruang-dapur",
+      "name": "Ruang Dapur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1524,
+      "initialViewParameters": {
+        "yaw": -2.408984738312107,
+        "pitch": 0.14273010788389406,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5791097615514982,
+          "pitch": 0.18187304253175007,
+          "rotation": 0,
+          "target": "3-ruang-utama"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0563636075695477,
+          "pitch": -0.14095897873738394,
+          "title": "Kulkas",
+          "text": "Lengkap dengan kulkas modern ala-ala koki di kolam ikan"
+        },
+        {
+          "yaw": -1.010334529186924,
+          "pitch": 0.07986095355639122,
+          "title": "Sirkulasi",
+          "text": "Posisi dapur yang ada di pinggir rumah.<div><br></div><div>Sangat baik untuk sirkulasi udara dan keamanan dalam memasak, serta membuat pengen tetangga dengan aroma masakan kita :)</div>"
+        },
+        {
+          "yaw": -0.44350315403106144,
+          "pitch": -0.07422667169131358,
+          "title": "LCD",
+          "text": "Agar tidak ketinggalan update infotainment saat memasak di dapur"
+        }
+      ]
+    },
+    {
+      "id": "2-halaman-luar",
+      "name": "Halaman Luar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1403,
+      "initialViewParameters": {
+        "yaw": -2.23412416435138,
+        "pitch": -0.16705663923869452,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22091503008935653,
+          "pitch": 0.29764502858693653,
+          "rotation": 0,
+          "target": "3-ruang-utama"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.695055010823136,
+          "pitch": -0.06949452077581597,
+          "title": "Pemandangan alam",
+          "text": "Pantai Banyu Tibo, juga memiliki air terjun yang berada di tebing pantai.<div><br></div><div>Nah, selain pemandangan alam yang indah kamu juga bisa merasakan sensasi bermain air laut sekaligus air terjun yang ada di sana.</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-ruang-utama",
+      "name": "Ruang Utama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.7332227070776511,
+        "pitch": 0.1436655530742854,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.560511876824859,
+          "pitch": 0.2764847077069579,
+          "rotation": 0,
+          "target": "2-halaman-luar"
+        },
+        {
+          "yaw": -2.4516085467218485,
+          "pitch": 0.05466725165177522,
+          "rotation": 0,
+          "target": "0-ekstra-kamar"
+        },
+        {
+          "yaw": 0.020249115955952135,
+          "pitch": 0.1770294822187708,
+          "rotation": 0,
+          "target": "1-ruang-dapur"
+        },
+        {
+          "yaw": -1.0409388281205185,
+          "pitch": 0.25213178280293036,
+          "rotation": 0,
+          "target": "4-ruang-keluarga"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1269313843728384,
+          "pitch": 0.2243156523322476,
+          "title": "Ruang Minimalis",
+          "text": "Dapat digunakan untuk menerima tamu sementara.<div><br></div><div>Selain dekat dengan pintu rumah, dengan pencahayaan langsung dari luar akan membuat nyaman suasana di dalam rumah.</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-ruang-keluarga",
+      "name": "Ruang Keluarga",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.7858597476965024,
+        "pitch": 0.056900352062566384,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.044223771463119,
+          "pitch": 0.03430114395847994,
+          "title": "Ruang Keluarga",
+          "text": "Yang pasti masuk ke ruangan ini sudah dianggap keluarga oleh Tuan rumah.<div><br></div><div>Dan pasti merasa nyaman dan malas pulang bagi yang bertamu :D</div>"
+        }
+      ]
+    }
+  ],
+  "name": "SiRecon Demo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
